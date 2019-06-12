0c8cf050550654e36d980646cb4e13284a1aa729
Sarah McGivern <sarah.mcgivern12@gmail.com>
2019-06-12 19:03:35 -0400

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Api } from './services/api.service';
+
+interface MovieInfo {
+  title: string;
+}
+
+interface Movie {
+  movie: MovieInfo[];
+}
+
+interface ApiData {
+  results: Movie;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +21,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Movie-Database';
+  movies: Movie[];
+
+  //  ngOnInit() {
+  //   this.api.detail.subscribe(data => console.log(data));
+  // }
+
+  constructor(private api: Api, private router: Router) {}
+
+
+
+  ngOnInit() {
+    this.api.getMovie().subscribe(data => console.log(data));
+  }
+
+  // ngOnInit() {
+  //   this.api.movies.subscribe(data => this.movies = data);
+  // }
+
+  // getAllMovies = () => {
+  //   this.api
+  //   .subscribe(data => console.log('DATA FROM FACT CALL', data));
+
+  //   this.api.getMovie().subscribe((data: ApiData) => {
+  //     console.log('GETTING DATA');
+  //     this.list = data.results; 
+  //     this.errorMessage = null;
+  //   }
+
+  // };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
+import { HttpClientModule } from '@angular/common/http'
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+
+import { Api } from './services/api.service';
+
+
+const appRoutes: Routes = [
+  {path: '', component: AppComponent},
+  // {path: 'favorites', component: }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [Api],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
