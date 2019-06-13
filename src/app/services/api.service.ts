b24e5633ae0eeb0b0f2ec0e689ee9465b96569c3
Robert Dimech <robertdimech@Roberts-MacBook-Pro-13.local>
2019-06-12 20:09:03 -0400

    api work

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,26 +1,22 @@
 import {Injectable} from '@angular/core'
 import {HttpClient} from '@angular/common/http'
 import { BehaviorSubject } from 'rxjs';
+import {HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 
 export class Api {
-    apiUrl = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/20e97e1117msh94521a8d54a51d0p1a1eecjsn0736ce7afe60'
-    // appId: string = '00a41518';
-    // appKey: string = '20e97e1117msh94521a8d54a51d0p1a1eecjsn0736ce7afe60';
-   
-    
-    // private _recipes = new BehaviorSubject([]);
-    // recipes = this._recipes.asObservable();
+    apiUrl = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?country=us&term=office'
 
     constructor( private http: HttpClient) {}
     
-    // getMovie = (query) => {
-    //     return this.http.get(this.movieUrl = `https://api.edamam.com/search?q=${query || ""}&app_id=${this.appId}&app_key=${this.appKey}&from=${pagFrom}&to=${pagTo}&health=${healthValue}&ingr=${numberIngr}`)
-    // }
 
     getMovie = () => {
-        return this.http.get(this.apiUrl);
+        let headers = new HttpHeaders({
+            "X-RapidAPI-Host":"utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+            "X-RapidAPI-Key": "20e97e1117msh94521a8d54a51d0p1a1eecjsn0736ce7afe60"
+        });
+        return this.http.get(this.apiUrl, { headers });
     }
 
 
