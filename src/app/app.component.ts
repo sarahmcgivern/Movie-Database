0af2ed1ec3b01b55620d61b13e63d673b0040715
jennyjohnston <jenny4johnston@gmail.com>
2019-06-17 22:00:15 -0400

    styling, imdb call/function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,25 @@ interface Movie {
   count: number;
   movie: MovieInfo[];
 }
+
+// interface IMDBInfo {
+
+// }
+
+interface IMDB {
+  filmid: string;
+  genre: string;
+  plot: string;
+  poster: string;
+  // released: Url;
+  runtime: string;
+}
+
 interface ApiData {
   items: Movie;
   hits: Movie[];
+  imdbResults: IMDB;
+  results: IMDB[];
 }
 
 @Component({
@@ -20,15 +36,19 @@ interface ApiData {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Movie-Database';
+  movie:any;
   // movies: Movie[];
+  list: object[];
   searchInput: string;
   typeMovie: boolean = false;
   typeTv: boolean = false;
   videoType: string;
   startYear: string = '1900';
   endYear: string = '2019';
+  imdbId: string = '80998296';
   locations = [
     {id: '23', name:'Australia'},
     {id: '33', name: 'Canada'},
@@ -72,6 +92,26 @@ export class AppComponent {
     // this.api.getMovie(this.searchInput).subscribe(data => console.log(data));
     this.api.getMovie(this.videoType, this.startYear, this.endYear,this.selectedLocation).subscribe(data => console.log(data));
   }
+
+
+getAllImdbDetails = () => {
+  if (this.typeMovie === true && this.typeTv === false) {
+    this.videoType = 'Movie'; 
+    } else {
+      if (this.typeMovie === false && this.typeTv === true){
+        this.videoType = 'Series';
+      } else this.videoType = 'Any';
+    }
+  // this.api.getImdbDetails(this.imdbId).subscribe(data => console.log(data));
+   this.api.getImdbDetails(this.imdbId).subscribe(data => {
+     this.movie=data;
+    console.log(this.movie)
+   })
+
+  // (data:ApiData) => {
+  //   this.list = data;
+  // }
+}
 
   // ngOnInit() {
   //   this.api.movies.subscribe(data => this.movies = data);
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -16,4 +16,14 @@ export class Api {
         });
         return this.http.get(this.apiUrl + 'q=' + 'get%3Anew7' + '-!' + startYear + ',' + endYear + '-!0,5-!0,10-!0-!' + videoType + '-!Any-!Any-!gt0-!{downloadable}&t=ns&cl=' + country + '&st=adv&ob=Relevance&p=1&sa=and', { headers });
     }
+
+    // 'or getimdb?'
+    getImdbDetails = (imdbId) => {
+        let headers = new HttpHeaders({
+            "X-RapidAPI-Host":"unogs-unogs-v1.p.rapidapi.com",
+            "X-RapidAPI-Key": "20e97e1117msh94521a8d54a51d0p1a1eecjsn0736ce7afe60"
+        });
+        return this.http.get(this.apiUrl + 't=getimdb' + '&q=' + imdbId, { headers });
+    }
+
 }
